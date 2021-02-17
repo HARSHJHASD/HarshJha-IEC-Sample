a48078dbcf2d310555985e12effd441cf3b16a1a
harshjhasd <harshjha19101997@gmail.com>
2021-02-17 11:58:00 +0530

    aarju upoad zip

--- aboutiec.php
+++ aboutiec.php
@@ -102,133 +102,109 @@
 
 
 
-<div class="page-heading">
-  <h2>
-    About Us
-  </h2>
-</div>
+<section>
+						<div class="inner-page-top-section animatedParent">
+							<div class="container">
+								<div class="page-heading">
+									<h2>
+										About Us
+									</h2>
+								</div>
+							</div>
+						</div>
+					</section>
 
 
-
-<!-- about iec section -->
 
 <section>
-  <div class="inner-page-middle-section animatedParent">
-    <div class="container">
-      <div class="about-us-main-div">
-        <p>
-          IEC Group, found its genesis way back in the year 1981 when Sh. R.L. Gupta,
-          laid the stone to found India Education Centre under his aegis with a dream
-          to make India leader in <a href="http://www.iec.edu.in/courses/dual-courses/masters-degree-in-computer-applications-mca">Information Technology</a> .Soon it become the frontrunner
-          in imparting IT Education. In year 1994 with aim of expansion it was corporatized
-          and India Education Centre Ltd. was founded later named IEC Softwares Ltd.
-          and now IEC Education Ltd. (ISO:9001) .IEC with a market capitalization of over
-          50 crore rupees one of the pioneers in India, in the field of Information Technology
-          has been a frontrunner in imparting IT training and Education at the length and
-          breadth of the Country. IEC Education Limited is Listed on Bombay, Delhi, Jaipur
-          Stock Exchanges.
-        </p><br>
-        <p>
-          The Company has been conceived, with the ambitious objectives of developing professional expertise and skilled manpower in Information Technology (IT) and related areas. This will enable the IT Professionals to exploit efficiently the
-          emerging opportunities, and meet economic challenges being thrown upon by the rapid global IT revolution, which is influencing virtually every area of development and social activity.
-        </p>
-        <br>
-        <p>
-          IEC has also been awarded various core ICT projects by Government of Madhya Pradesh, Rajasthan Government, Delhi Government taking the IEC aim of IT revolution from the flesh of cities to various small towns and rural areas. IEC has
-          trained more than 10000 students through these programs
-        </p>
-        <br>
-        <p>
-          IEC has enjoyed its global presence with 7 branches and over 275 franchisee centres in India and in countries like Nepal, Maldives, Singapore and Oman; It provided a wide spectrum of unique Information Technology solutions and services to
-          a clientele of premier organisations in the government.
-        </p>
-        <br>
-        <p>
-          Today IEC is an established brand name in <a href="http://www.iec.edu.in/courses/dual-courses/masters-degree-in-computer-applications-mca">Computer Education</a>,<a href="http://www.iec.edu.in/b-pharm-course/"> Science</a>, <a
-            href="http://www.iec.edu.in/courses/dual-courses/masters-of-applied-management-mam">Technology and Management</a>, <a href="http://www.iec.edu.in/courses/graduate-courses/bachelor-of-hotel-management-catering-technology">Hotel
-            Management</a>, <a href="http://www.iec.edu.in/b-pharm-course/">Pharma</a>, Vocational education, and an organization of national repute.
-        </p>
-        <br>
-        <div class="about-us-text-with-heading">
-          <strong>
-            Global Projects Successfully Completed by IEC in the Past
-          </strong>
-          <p>
-            IEC Education had emerged as a conglomerate of diverse IT activities ranging from Software development to consultancy, offshore and on-site development, training and Turnkey Solutions and had planned overseas strategic alliances with a
-            thrust on USA, UK , Poland and Malaysian market .
-          </p>
-        </div><br>
-
-        <div class="about-us-text-with-heading">
-          <strong>
-            Malaysia
-          </strong>
-          <p>
-            IEC was a part of Multimedia Super Corridor (MSC), a mega project of Malaysian government .The US $ 5.3 billion MSC being built on more than 750 sq.km was aimed at serving as a platform for leading MNCs and Software companies of the
-            world for technology transfer. MSC functioned as an island of excellence with multimedia specific capabilities, technologies, infrastructure, legislation, policies and systems for competitive advantages. IEC had been spearheading
-            expertise in the flagship application for Smart Education System (SES), Borderless Marketing, and User Interface Application.
-          </p>
-        </div><br>
-
-        <div class="about-us-text-with-heading">
-          <strong>
-            IEC Infotech Sdn.Bhd. IEC in Malaysia (MSC Pioneer Status)
-          </strong>
-          <p>
-            IEC Infotech was a 100 % wholly owned subsidiary of IEC Softwares Limited, committed towards providing comprehensive and cost effective solutions and services to organizations to improve their competitiveness and competencies.
-          </p>
-        </div><br>
-
-        <div class="about-us-text-with-heading">
-          <strong>
-            Malaysian Graduate School of Management (MGSM)
-          </strong>
-          <p>
-            Successfully developed and installed “College Management System”. This system was an integrated solution of: Admission System, Academic System, Human Resource Management &amp; Finance System.
-          </p>
-        </div>
-        <br>
-
-        <div class="about-us-text-with-heading">
-          <strong>
-            Meteor Sdn. Bhd.
-          </strong>
-          <p>
-            Designed and developed the “Integrated Financial Management System (IFMS)”. This solution collected the financial data of all the subsidiary companies of Meteor and generate the required financial and MIS reports for individual
-            subsidiaries as well as the consolidated financial reports for the whole group.Desk Project (State Govt of Johor Baru) Developed web based IT course content for, Desk Project of State Govt. of Johar Baru. This project was jointly
-            executed with MOL.COM.Sultana Amina Hospital, Johar Baru Customising, Designing and installing Intelligent Hospital Management System.University Putra Malaysia, Malaysia
-            <br>
-            IT education programs for the students of different faculties of UPM.Sunway College, Malaysia High end IT training through “ACDS”, the professional Development wing of Stamford College, Malaysia.
-          </p>
-        </div> <br>
-
-        <div class="about-us-text-with-heading">
-          <strong>
-            Poland
-          </strong>
-          <p>
-            IEC had signed a MOU with Polservice sp. Zoo. of Warsaw in the field of Information Technology Solutions for Poland. Polservice was engaged in the area of transferring technologies, knowledge and technical assistance from global sources
-            into Poland. As per the MOU, IEC provided IT solutions for Wireless Application Protocols (WAP), Web Business, Finance, Manufacturing, and utilities &amp; Services. It also trained Polish IT professionals in the use of varied and highly
-            sophisticated software systems.
-          </p>
-        </div>
-        <br>
-
-        <div class="about-us-text-with-heading">
-          <strong>
-            Milestones in the field of Education Sector Global Ventures
-          </strong>
-          <p>
-            IEC Dynamics Pte, a wholly owned subsidiary of the Company in Singapore, is to be incorporated with the objective of establishing, takeover, run and manage Educational Academy, Institutions, centers, schools, colleges for imparting all
-            type of education to students for various courses and competitive exams including medical, Dental, Pharmacy, Engineering, Polytechnic, Information technology, Management, hotel &amp; Tourism Management, Fashion Technology and all types
-            of academic, professional, vocational, technical or computer courses for exams conducted by various Board / Universities or Institutes. Milestones in the field of Education Sector.
-          </p>
-        </div> <br>
-      </div>
-    </div>
-  </div>
-</section>
-
-
+						<div class="inner-page-middle-section animatedParent">
+							<div class="container">
+								<div class="about-us-main-div">
+									<p>
+										IEC Group, found its genesis way back in the year 1981 when Sh. R.L. Gupta,
+										laid the stone to found India Education Centre under his aegis with a dream
+										to make India leader in <a href="http://www.iec.edu.in/courses/dual-courses/masters-degree-in-computer-applications-mca">Information Technology</a> .Soon it become the frontrunner
+										in imparting IT Education. In year 1994 with aim of expansion it was corporatized
+										and India Education Centre Ltd. was founded later named IEC Softwares Ltd.
+										and now IEC Education Ltd. (ISO:9001) .IEC with a market capitalization of over
+										50 crore rupees one of the pioneers in India, in the field of Information Technology
+										has been a frontrunner in imparting IT training and Education at the length and
+										breadth of the Country. IEC Education Limited is Listed on Bombay, Delhi, Jaipur
+										Stock Exchanges.
+									</p>
+									<p>
+										The Company has been conceived, with the ambitious objectives of developing professional expertise and skilled manpower in Information Technology (IT) and related areas. This will enable the IT Professionals to exploit efficiently the emerging opportunities, and meet economic challenges being thrown upon by the rapid global IT revolution, which is influencing virtually every area of development and social activity.
+									</p>
+									<p>
+										IEC has also been awarded various core ICT projects by Government of Madhya Pradesh, Rajasthan Government, Delhi Government taking the IEC aim of IT revolution from the flesh of cities to various small towns and rural areas. IEC has trained more than 10000 students through these programs
+									</p>
+									<p>
+										IEC has enjoyed its global presence with 7 branches and over 275 franchisee centres in India and in countries like Nepal, Maldives, Singapore and Oman; It provided a wide spectrum of unique Information Technology solutions and services to a clientele of premier organisations in the government.
+									</p>
+									<p>
+										Today IEC is an established brand name in <a href="http://www.iec.edu.in/courses/dual-courses/masters-degree-in-computer-applications-mca">Computer Education</a>,<a href="http://www.iec.edu.in/b-pharm-course/"> Science</a>, <a href="http://www.iec.edu.in/courses/dual-courses/masters-of-applied-management-mam">Technology and Management</a>, <a href="http://www.iec.edu.in/courses/graduate-courses/bachelor-of-hotel-management-catering-technology">Hotel Management</a>, <a href="http://www.iec.edu.in/b-pharm-course/">Pharma</a>, Vocational education, and an organization of national repute.
+									</p>
+									<div class="about-us-text-with-heading">
+										<strong>
+											Global Projects Successfully Completed by IEC in the Past
+										</strong>
+										<p>
+											IEC Education had emerged as a conglomerate of diverse IT activities ranging from Software development to consultancy, offshore and on-site development, training and Turnkey Solutions and had planned overseas strategic alliances with a thrust on USA, UK , Poland and Malaysian market .
+										</p>
+									</div>
+									<div class="about-us-text-with-heading">
+										<strong>
+											 Malaysia
+										</strong>
+										<p>
+											IEC was a part of Multimedia Super Corridor (MSC), a mega project of Malaysian government .The US $ 5.3 billion MSC being built on more than 750 sq.km was aimed at serving as a platform for leading MNCs and Software companies of the world for technology transfer. MSC functioned as an island of excellence with multimedia specific capabilities, technologies, infrastructure, legislation, policies and systems for competitive advantages. IEC had been spearheading expertise in the flagship application for Smart Education System (SES), Borderless Marketing, and User Interface Application.
+										</p>
+									</div>
+									<div class="about-us-text-with-heading">
+										<strong>
+											 IEC Infotech Sdn.Bhd. IEC in Malaysia (MSC Pioneer Status)
+										</strong>
+										<p>
+											IEC Infotech was a 100 % wholly owned subsidiary of IEC Softwares Limited, committed towards providing comprehensive and cost effective solutions and services to organizations to improve their competitiveness and competencies.
+										</p>
+									</div>
+									<div class="about-us-text-with-heading">
+										<strong>
+											 Malaysian Graduate School of Management (MGSM)
+										</strong>
+										<p>
+											Successfully developed and installed “College Management System”. This system was an integrated solution of: Admission System, Academic System, Human Resource Management &amp; Finance System.
+										</p>
+									</div>
+									<div class="about-us-text-with-heading">
+										<strong>
+											 Meteor Sdn. Bhd.
+										</strong>
+										<p>
+											Designed and developed the “Integrated Financial Management System (IFMS)”. This solution collected the financial data of all the subsidiary companies of Meteor and generate the required financial and MIS reports for individual subsidiaries as well as the consolidated financial reports for the whole group.Desk Project (State Govt of Johor Baru) Developed web based IT course content for, Desk Project of State Govt. of Johar Baru. This project was jointly executed with MOL.COM.Sultana Amina Hospital, Johar Baru Customising, Designing and installing Intelligent Hospital Management System.University Putra Malaysia, Malaysia
+											<br>
+											IT education programs for the students of different faculties of UPM.Sunway College, Malaysia High end IT training through “ACDS”, the professional Development wing of Stamford College, Malaysia.
+										</p>
+									</div>
+									<div class="about-us-text-with-heading">
+										<strong>
+											Poland
+										</strong>
+										<p>
+											IEC had signed a MOU with Polservice sp. Zoo. of Warsaw in the field of Information Technology Solutions for Poland. Polservice was engaged in the area of transferring technologies, knowledge and technical assistance from global sources into Poland. As per the MOU, IEC provided IT solutions for Wireless Application Protocols (WAP), Web Business, Finance, Manufacturing, and utilities &amp; Services. It also trained Polish IT professionals in the use of varied and highly sophisticated software systems.
+										</p>
+									</div>
+									<div class="about-us-text-with-heading">
+										<strong>
+											Milestones in the field of Education Sector Global Ventures
+										</strong>
+										<p>
+											IEC Dynamics Pte, a wholly owned subsidiary of the Company in Singapore, is to be incorporated with the objective of establishing, takeover, run and manage Educational Academy, Institutions, centers, schools, colleges for imparting all type of education to students for various courses and competitive exams including medical, Dental, Pharmacy, Engineering, Polytechnic, Information technology, Management, hotel &amp; Tourism Management, Fashion Technology and all types of academic, professional, vocational, technical or computer courses for exams conducted by various Board / Universities or Institutes. Milestones in the field of Education Sector.
+										</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</section>
 
 <?php  include 'template/footer.php';  ?>
